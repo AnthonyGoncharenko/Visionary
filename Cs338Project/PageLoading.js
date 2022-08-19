23c802882bb6e0a810f5cc81704e21c03ae00df3
PeachesAndPears <43767395+PeachesAndPears@users.noreply.github.com>
2022-08-18 20:42:34 -0400

    killing the dead folder

--- Cs338Project/PageLoading.js
+++
@@ -1,8 +0,0 @@
-window.onload = $(function() {
-    var loc = window.location.href;
-       $(".navbar .navbar-nav > li").each(function() {
-            if (loc.match('/home')) {
-                $(this).addClass("active"); 
-            }
-       });
-});
