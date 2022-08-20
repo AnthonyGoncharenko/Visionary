731619c32de375a4c9ed264f83324eb0edffb36a
PeachesAndPears <43767395+PeachesAndPears@users.noreply.github.com>
2022-08-20 16:58:22 -0400

    fixerupper forms

--- app.py
+++ app.py
@@ -103,6 +103,8 @@ def sign_up_page():
 ########################################################################
 @app.route('/makepost', methods=['GET', 'POST'])
 def make_post_page():
+    if 'user' in session and request.method == 'POST':
+        get_db().create_post(session['user'], request.form['post_title'], request.form['post_content'], request.form['post_image'])
     if 'user' in session:
         form = PostForm()
         session['user_details'] =  get_db().get_user(session['user'])
@@ -117,12 +119,12 @@ def make_post_page():
 ########################################################################
 #                         VIEW POST PAGE
 ########################################################################
-@app.route('/view_post/<int:pid>', methods=['GET'])
-def view_post_page(pid):
-    if request.method == 'GET':
+@app.route('/view_post', methods=['GET'])
+def view_post_page():
+    if request.method == 'GET' :
         if 'user' in session:
             session['user_details'] =  get_db().get_user(session['user'])
-        return render_template("ViewPost.html", session=session, canDelete=canDelete(pid))
+        return render_template("ViewPost.html", session=session, postId = request.args.get('post_id'), canDelete=canDelete(request.args.get('post_id')))
     else:
         return redirect(url_for('home_page'))
 ########################################################################
@@ -260,9 +262,9 @@ def close_connection(exception):
 def post_to_dict(post):
     m = {}
     m['pid'] = post['pid']
-    m['author'] = post['author']
+    m['uid'] = post['uid']
     m['title'] = post['title']
-    m['data'] = post['data']
+    m['content'] = post['content']
     return m
 
 def trending_posts():
--- database.py
+++ database.py
@@ -134,11 +134,12 @@ class Database:
         data = self.__select('SELECT * FROM users WHERE uid=?', [uid])
         if data:
             return user_to_dict(data[0])
+
     def create_post(self, username, title, content, img):
         if (user := self.get_user(username)) is None:
             return 
         self.create_img(user['user_id'], img)
-        imid = self.__get_img_id(user['user_id'], img)[0]
+        imid = self.__get_img_id(user['user_id'], img)[0][0]
         self.__execute('INSERT INTO posts (uid, title, content, imid) VALUES (?, ?, ?, ?)', [user['user_id'], title, content, imid])
 
         pid = self.__select("SELECT pid FROM posts WHERE uid=? AND title=? AND content=? AND imid=?", [user['user_id'], title, content, imid])
@@ -147,15 +148,16 @@ class Database:
         if str(pid) not in posts:
             posts.append(str(pid))
             new_posts = " ".join(posts)
-            self.__execute("UPDATE users SET posts=? WHERE uid=?", [new_posts, uid])
+            self.__execute("UPDATE users SET posts=? WHERE uid=?", [new_posts, user['user_id']])
 
     def delete_post(self, pid):
         self.__execute("DELETE FROM posts where pid=?", [pid])
+
     def create_img(self, uid, img):
         self.__execute('INSERT INTO images (uid, img) VALUES (?, ?)', [uid, img])
 
     def __get_img_id(self, uid, img):
-        return self.__select('SELECT FROM images WHERE uid=? AND img=?', [uid, img])
+        return self.__select('SELECT imid FROM images WHERE uid=? AND img=?', [uid, img])
 
     def get_posts_ids_by_author(self, author):
         data = self.__select('SELECT posts FROM users WHERE username=?', [author])
