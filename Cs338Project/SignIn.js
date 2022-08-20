2be8548626d5cf8e8510710b13a8160fe938b88c
PeachesAndPears <43767395+PeachesAndPears@users.noreply.github.com>
2022-08-20 01:42:12 -0400

    fuck my life

--- Cs338Project/SignIn.js
+++
@@ -1,7 +0,0 @@
-function SubmiSignInFunc() {
-  document.getElementById("SignIn").submit();
-}
-
-function SignUpRedirect() {
-  location.href = 'new_url'; //put the location of sign up page
-}
--- routes.py
+++ routes.py
@@ -4,27 +4,20 @@ import os
 from pprint import pprint
 from threading import Thread
 from time import sleep
-
 from flask import Blueprint, g, redirect, render_template, request, url_for, session
 from passlib.hash import pbkdf2_sha256
-
 from database import Database
 from DocumentUploadForm import DocumentUploadForm
 from SignInForm import SignInForm
 from SignUpForm import SignUpForm
-
 # backend = Blueprint('server', __name__)
-
 home = Blueprint('home', __name__, static_folder='static')
 signup = Blueprint('signup', __name__, static_folder='static')
 signin = Blueprint('signin', __name__, static_folder='static')
 logout = Blueprint('logout', __name__)
-
 # ALLOWED_EXTENSIONS = {'tiff', 'svs', 'jpg', 'jpeg', 'png', 'JPG', 'JPEG', 'PNG'}
-
 # def allowed_file(File):
 #     return '.' in File.filename and File.filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 ########################################################################
 #                           GET DATABASE
 ########################################################################
@@ -38,9 +31,6 @@ def get_db():
 ########################################################################
 #                         END GET DATABASE
 ########################################################################
-
-
-
 ########################################################################
 #                           SIGN IN PAGE
 ########################################################################
@@ -66,8 +56,6 @@ def sign_in_page():
 ########################################################################
 #                         END SIGN IN PAGE
 ########################################################################
-
-
 ########################################################################
 #                           SIGN UP PAGE
 ########################################################################
@@ -77,60 +65,67 @@ def sign_up_page():
     if request.method == 'POST':
         if form.validate_on_submit():
             encrypted_password = pbkdf2_sha256.encrypt(request.form['password'],rounds=200000, salt_size=16)
-
             print("CHECKING THE DATABASE FOR THE USER DATA...")
-
             if (response := get_db().get_user(request.form['username'])) is not None:
                 print('USER ALREADY EXISTS IN DATABASE...')
                 print('REDIRECTING TO SIGNUP')
                 return render_template("SignUp.html", form=SignUpForm(), error_messages = ['USER ALREADY EXISTS IN DATABASE'])
-
             print("ADDING USER TO THE DATABASE...")
-
             get_db().create_user(request.form['username'], encrypted_password, request.form['email'])
-
             print("REDIRECTING TO THE SIGNIN...")
-
             return redirect(url_for("signin.sign_in_page"))
     return render_template("SignUp.html", form=form)
 ########################################################################
 #                         END SIGN UP PAGE
 ########################################################################
-
-
 ########################################################################
 #                           HOME PAGE
 ########################################################################
-@home.route('/home', methods=['GET', 'POST'])
+@home.route('/home', methods=['GET'])
 def home_page():
     return render_template("Home.html")
 ########################################################################
 #                         END HOME PAGE
 ########################################################################
-
-
 ########################################################################
 #                           MAKE POST PAGE
 ########################################################################
-@home.route('/post', methods=['GET', 'POST'])
+@home.route('/makepost', methods=['GET'])
 def post_page():
     return render_template("MakePost.html")
 ########################################################################
 #                         MAKE POST PAGE
 ########################################################################
-
-
+########################################################################
+#                           VIEW POST PAGE
+########################################################################
+@home.route('/post', methods=['GET', 'POST', 'DELETE'])
+def view_post_page():
+    post_id = request.args.get('post_id')
+    return render_template("ViewPost.html", postId = post_id)
+########################################################################
+#                         VIEW POST PAGE
+########################################################################
 ########################################################################
 #                           FIND AUTHORS PAGE
 ########################################################################
-@home.route('/authors', methods=['GET', 'POST'])
+@home.route('/authors', methods=['GET', 'POST', 'DELETE'])
 def authors_page():
-    return render_template("FindAuthors.html")
+    user = request.args.get('user')
+    return render_template("FindAuthors.html", user = user)
 ########################################################################
 #                         FIND AUTHORS PAGE
 ########################################################################
-
-
+########################################################################
+#                           PROFILE PAGE
+########################################################################
+@home.route('/profile', methods=['GET'])
+def profile_page():
+    user = request.args.get('user')
+    return render_template("Profile.html", user = user)
+########################################################################
+#                         PROFILE PAGE
+########################################################################
 ########################################################################
 #                           LOG OUT
 ########################################################################
@@ -142,15 +137,11 @@ def logout_page():
 ########################################################################
 #                         END LOG OUT
 ########################################################################
-
-
-
 # @home.route('/')
 # @home.route('/home')
 # def home_page():
 #     form = DocumentUploadForm()
 #     return render_template("home.html", form=form, loadingbar=False)
-
 # @backend.route('/server', methods=['GET', 'POST'])
 # def server():
 #     form = DocumentUploadForm()
@@ -161,7 +152,6 @@ def logout_page():
 #             diagnosis_thread = Thread(target=do_diagnosis, args=("",))
 #             diagnosis_thread.start()
 #     return render_template("diagnosis.html")
-
 # @backend.get('/modelStatus')
 # def modelStatus():
 #     status_file = "diagnosis_status.json"
@@ -172,8 +162,6 @@ def logout_page():
 #             with open(status_file) as f:
 #                 return json.load(f)
 #     return get_status()
-
-
 # def do_diagnosis(path_to_image: str, model_name: str = "dummy_model"):
 #     print("Starting diagnosis function")
 #     status = {}
